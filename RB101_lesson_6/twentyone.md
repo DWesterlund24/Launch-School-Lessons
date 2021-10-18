3f23951e626eea020918f9d15bba6640aa096918
Daniel Westerlund <DWesterlund24@gmail.com>
2021-10-18 16:24:06 -0400

    Add bonus features to twentyone in lesson_6

--- RB101_lesson_6/twentyone.md
+++ RB101_lesson_6/twentyone.md
@@ -1,9 +1,9 @@
 # Problem
-We are creating a game called twenty-one in which the player tries to get their cards to value as close to 21 as possible without going over. If the player goes over 21, they auromatically lose. The game consists of a dealer and a player.
+We are creating a game called twenty-one in which the player tries to get their cards to value as close to 21 as possible without going over. If the player goes over 21, they automatically lose. The game consists of a dealer and a player.
 ### Explicit Requirements
 - Start with normal 52-card deck
 - Both participants are initially dealt 2 cards
-- Participants have one card idden from the other participants
+- Participants have one card hidden from the other participants
 #### Card values
 - Numbers 2 through 10 are worth their face value
 - Jack, Queen, and King are all worth 10
@@ -11,10 +11,9 @@ We are creating a game called twenty-one in which the player tries to get their 
 #### Player turn
 - The player goes first
 - On the player's turn they can decide to either 'hit' or 'stay'.
-  - Hit means they get an addition card.
+  - Hit means they get an additional card.
   - Stay means they lock in their current hand.
 #### Dealer turn
-- When the player stays, it's the dealer's turn
 - The dealer should hit until their total is at least 17, at which point they should stay.
 #### Comparing cards
 - When both players stay and neither have gone over 21, the higher value wins
@@ -50,3 +49,23 @@ We are creating a game called twenty-one in which the player tries to get their 
 # Non Required Additions
 - We can ask the player if they'd like to continue playing after each round
 - Instead of reshuffling the deck everytime, we could wait until we've gone through the whole deck to reshuffle by adding a discard pile. This would of course only come into play if the player decides to keep playing.
+
+# Bonus Features
+1. To avoid repeated calculation of hands, we should have a working total that adds the value of each card. To implement this, we could also have an ace tally so we know how many times we can deduct 10 from the score.
+
+2. > We use the play_again? three times: after the player busts, after the dealer busts, or after both participants stay and compare cards. Why is the last call to play_again? a little different from the previous two?
+
+    The method itself here does not change but in the previous instances we are skipping over the rest of the code in the loop.
+
+# Adjusted Algorithm
+- Setup deck and hands as specified in data structure
+- Deal cards one at a time alternating beginning with the player to start the game, both players should then have 2 cards
+- Show hands to the user
+  - Show the dealers first card and show all subsequent cards as unknown cards
+  - Show all of the players cards
+- Allow player to hit or stay
+- Computer should hit if below 17, otherwise stay
+- After every card, add card value to hand total, if card is an ace, add 1 to ace tally.
+- Check to see if hand total is over 21 if it is....
+  - If an ace tally is positive, deduct 10 from total and 1 from ace tally, continue the game
+  - If ace tally is not positive, display what player is over 21 and declare the other player the winner
--- RB101_lesson_6/twentyone.rb
+++ RB101_lesson_6/twentyone.rb
@@ -2,6 +2,8 @@ CARDS_IN_SUIT = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
                   '8' => 8, '9' => 9, '10' => 10, 'jack' => 10, 'queen' => 10,
                   'king' => 10, 'ace' => 11 }
 
+TARGET_NUMBER = 27
+
 def prompt(string)
   puts('=> ' + string)
 end
@@ -13,7 +15,8 @@ def create_deck(array)
     suit = CARDS_IN_SUIT.map do |key, value|
       [char + key, value]
     end.to_h
-  object.merge!(suit)
+
+    object.merge!(suit)
   end
 end
 
@@ -64,31 +67,39 @@ def hit_or_stay?
   end
 end
 
-def add_up_hand(hand)
-  # Get card values
-  total = hand.each_with_object([]) do |card, score|
-    score << ALL_CARDS[card]
-  end
+def last_card_value(hand, total, ace_count)
+  last_card_value = ALL_CARDS[hand[-1]]
+  total += last_card_value
 
-  # Convert Aces value to 1 as necessary
-  loop do
-    if total.sum > 21
-      total[total.index(11)] = 1 if total.any?(11)
-    end
-
-    break if total.sum < 22 || total.none?(11)
-  end
-
-  total.sum
+  ace_count += 1 if last_card_value == 11
+  [total, ace_count]
 end
 
-def distinguish_winner(player_hand, dealer_hand)
-  player_score = add_up_hand(player_hand)
-  dealer_score = add_up_hand(dealer_hand)
+def convert_ace_if_needed(total, ace_tally)
+  loop do
+    if total > TARGET_NUMBER && ace_tally.positive?
+      total -= 10
+      ace_tally -= 1
+    else
+      break
+    end
+  end
+  [total, ace_tally]
+end
 
-  return 'Player wins!' if player_score > dealer_score
-  return 'Dealer wins!' if dealer_score > player_score
-  'Tie game!'
+def get_result(player_score, dealer_score)
+  return :player if player_score > dealer_score
+  return :dealer if dealer_score > player_score
+  :tie
+end
+
+def display_results(player_hand, player_score, dealer_hand, dealer_score)
+  puts ''
+  puts "=============="
+  prompt "Dealer has #{list_cards(dealer_hand)}, for a total of: #{dealer_score}"
+  prompt "Player has #{list_cards(player_hand)}, for a total of: #{player_score}"
+  puts "=============="
+  puts ''
 end
 
 def continue_playing?
@@ -101,11 +112,10 @@ def continue_playing?
 
     prompt 'Invalid input'
   end
-
 end
 
 prompt "Welcome to Twenty-one! The player with the most points wins!"
-prompt "But be careful, If you go over 21, you lose."
+prompt "But be careful, If you go over #{TARGET_NUMBER}, you lose."
 
 # Cards will be strings with the first character signifying suit
 # h5 => 5 of hearts, sking => King of spades, etc.
@@ -115,66 +125,104 @@ deck = ALL_CARDS.keys
 discard_pile = []
 
 loop do
-  player_hand = []
-  dealer_hand = []
+  player_games_won = 0
+  dealer_games_won = 0
 
-  2.times do
-    deal_card!(deck, player_hand, discard_pile)
-    deal_card!(deck, dealer_hand, discard_pile)
-  end
+  until player_games_won >= 5 || dealer_games_won >= 5
+    player_hand = []
+    dealer_hand = []
 
-  player_bust = false
-  dealer_bust = false
+    player_score = 0
+    dealer_score = 0
 
-  player_stayed = false
-  dealer_stayed = false
+    player_ace_count = 0
+    dealer_ace_count = 0
 
-  loop do
-    display_hands(player_hand, dealer_hand)
+    2.times do
+      deal_card!(deck, player_hand, discard_pile)
+      player_score, player_ace_count = last_card_value(player_hand, player_score, player_ace_count)
 
-    # Player turn
-    unless player_stayed
-      if hit_or_stay?.downcase.start_with?('h')
-        deal_card!(deck, player_hand, discard_pile)
-      else
-        player_stayed = true
+      deal_card!(deck, dealer_hand, discard_pile)
+      dealer_score, dealer_ace_count = last_card_value(dealer_hand, dealer_score, dealer_ace_count)
+    end
+
+    player_bust = false
+    dealer_bust = false
+
+    player_stayed = false
+    dealer_stayed = false
+
+    loop do
+      display_hands(player_hand, dealer_hand)
+
+      # Player turn
+      unless player_stayed
+        if hit_or_stay?.downcase.start_with?('h')
+          deal_card!(deck, player_hand, discard_pile)
+          player_score, player_ace_count = last_card_value(player_hand, player_score, player_ace_count)
+        else
+          player_stayed = true
+        end
+      end
+
+      player_score, player_ace_count = convert_ace_if_needed(player_score, player_ace_count)
+
+      if player_score > TARGET_NUMBER
+        player_bust = true
+        break
+      end
+
+      # Dealer turn
+      unless dealer_stayed
+        if dealer_score < (TARGET_NUMBER - 4)
+          deal_card!(deck, dealer_hand, discard_pile)
+          dealer_score, dealer_ace_count = last_card_value(dealer_hand, dealer_score, dealer_ace_count)
+        else
+          dealer_stayed = true
+        end
+      end
+
+      dealer_score, dealer_ace_count = convert_ace_if_needed(dealer_score, dealer_ace_count)
+
+      if dealer_score > TARGET_NUMBER
+        dealer_bust = true
+        break
+      end
+
+      break if player_stayed && dealer_stayed
+    end
+
+    display_hands(player_hand, dealer_hand, true)
+
+    # Display Winner
+    display_results(player_hand, player_score, dealer_hand, dealer_score)
+
+    if player_bust
+      prompt "Player bust, Dealer wins!"
+      dealer_games_won += 1
+    elsif dealer_bust
+      prompt "Dealer bust, Player wins!"
+      player_games_won += 1
+    else
+      case get_result(player_score, dealer_score)
+      when :player
+        prompt "Player wins!"
+        player_games_won += 1
+      when :dealer
+        prompt "Dealer wins!"
+        dealer_games_won += 1
+      when :tie
+        prompt "Tie!"
       end
     end
 
-    if add_up_hand(player_hand) > 21
-      player_bust = true
-      break
-    end
-
-    # Dealer turn
-    unless dealer_stayed
-      if add_up_hand(dealer_hand) < 17
-        deal_card!(deck, dealer_hand, discard_pile)
-      else
-        dealer_stayed = true
-      end
-    end
-
-    if add_up_hand(dealer_hand) > 21
-      dealer_bust = true
-      break
-    end
-
-    break if player_stayed && dealer_stayed
+    prompt "Player: #{player_games_won}, Dealer: #{dealer_games_won}"
   end
 
-  display_hands(player_hand, dealer_hand, true)
-
-  # Display Winner
-  puts ''
-  prompt "Player: #{add_up_hand(player_hand)}, Dealer: #{add_up_hand(dealer_hand)}"
-
-  if player_bust
-    prompt "Player bust, Dealer wins!"
-  elsif dealer_bust
-    prompt "Dealer bust, Player wins!"
+  if player_games_won >= 5
+    puts "You won! Congratulations!"
   else
-    prompt distinguish_winner(player_hand, dealer_hand)
+    puts "Dealer won."
   end
 
   break unless continue_playing?
