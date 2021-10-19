90e59faff503ea3fe69ae8476734ad142f5fc851
DWesterlund24 <85646546+DWesterlund24@users.noreply.github.com>
2021-10-18 21:16:40 -0400

    Delete RB101_lesson_5 directory

--- RB101_lesson_5/practice_1.rb
+++
@@ -1,5 +0,0 @@
-arr = ['10', '11', '9', '7', '8']
-
-p arr.sort { |a, b| b.to_i <=> a.to_i }
-
-p arr.sort_by { |el| el.to_i }.reverse
--- RB101_lesson_5/practice_8.rb
+++
@@ -1,9 +0,0 @@
-hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
-
-hsh.each_value do |value|
-  value.each do |string|
-    string.each_char do |char|
-      puts char if 'aeiou'.include?(char)
-    end
-  end
-end
